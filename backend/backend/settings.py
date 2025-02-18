30e8a49aecf7344fa15653a09feb904310c3eb33
Monyechi <63468925+Monyechi@users.noreply.github.com>
2025-02-18 11:01:55 +0700

    Configure CORS settings to allow requests from localhost and enable credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,7 +54,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
+
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 TEMPLATES = [
     {
